7adaa9613d5ca2e157972bd11957700988e77d0c
ksparkbr <ksparkbr@gmail.com>
2022-05-02 21:11:39 +0900

    cors 수정

--- src/config/global-config.js
+++ src/config/global-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     port : process.env.APP_PORT,
     cors : {
-        origin : [process.env.FRONT_ADDR1],
+        origin : [process.env.FRONT_ADDR1,process.env.FRONT_ADDR2],
         credentials : true,
     },
     mysql : {
